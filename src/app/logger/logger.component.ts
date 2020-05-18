c7206fb83a185b2ddfd698547f7fe2375d36edbc
Cezary <cez94k@gmail.com>
2020-05-18 10:07:39 +0200

    Logger component

---
+++ src/app/logger/logger.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logger',
+  templateUrl: './logger.component.html',
+  styleUrls: ['./logger.component.css']
+})
+export class LoggerComponent {
+
+  constructor() { }
+
+
+}
