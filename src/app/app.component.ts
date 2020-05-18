57defbfa3e7072cf3fe8111d701bd7d61c9f9867
Cezary <cez94k@gmail.com>
2020-05-18 11:20:56 +0200

    Log source data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`<app-logger></app-logger>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/logger/logger.component.ts
+++ src/app/logger/logger.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-logger',
-  templateUrl: './logger.component.html',
+  template: `
+    <ul>
+      <li *ngFor="let row of data">
+        {{ row }}
+      </li>
+    </ul>
+  `,
   styleUrls: ['./logger.component.css']
 })
 export class LoggerComponent {
+  data: string[] = [];
 
-  constructor() { }
-
+  constructor(public http: HttpClient) {
+    this.http.get('assets/SourceData.txt', {responseType: 'text'})
+      .subscribe(data => {debugger;
+        for (let line of data.split(/[\r\n]+/)){
+          this.data.push(line);
+        }
+      });
+  }
 
 }
